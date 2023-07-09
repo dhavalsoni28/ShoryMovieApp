40a5fab7f0272a181ead691c88487a74c3971d9d
Dhaval Soni <dhavalsoni1928@gmail.com>
2023-07-10 00:09:42 +0400

    Added Constants file for maintaining app level constants.

---
+++ ShoryApp/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ShoryApp
+//
+//  Created by Dhaval Soni on 09/07/2023.
+//
+
+import Foundation
+
+extension String {
+    static let omdbURL = "www.omdbapi.com"
+    static let omdbAPIKey = "24502cd1"
+    static let searchQueryString = "s"
+    static let omdbAPIKeyQueryString = "apikey"
+}
